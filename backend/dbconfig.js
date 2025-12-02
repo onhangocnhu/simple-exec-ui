0188525f5c3eaa34c91d64b47c78b47b72fa3aeb
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2025-12-02 02:16:47 +0000

    add sql files

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,7 +1,8 @@
 export const config = {
   user: 'myuser',
   password: '12341234',
-  server: '192.168.1.3',
+  // server: '192.168.1.3',
+  server: '10.229.174.141',
   port: 1433,
   database: 'RAPPHIM',
   options: {
