553582cdc85aab65e8b1f3c35d3a4a6084eb3a70
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2025-12-06 16:58:48 +0000

    update proc tracuusuatchieu

--- DBS/Procedure_WHERE_HAVING.sql
+++ DBS/Procedure_WHERE_HAVING.sql
@@ -2,31 +2,136 @@
 GO
 
 CREATE OR ALTER PROCEDURE sp_TraCuuLichChieu
-    @p_TenPhim NVARCHAR(255),
+    @p_TenPhim   NVARCHAR(255),
     @p_NgayChieu DATE
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT 
-        P.TenPhim,
-        R.TenRap,
-        PC.LoaiPhong,
-        SC.ThoiGianBatDau,
-        SC.ThoiGianKetThuc,
-        SC.DinhDangChieu, 
-        SC.TrangThai
-    FROM PHIM P
-    JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
-    JOIN RAP R ON SC.MaRap = R.MaRap
-    JOIN PHONGCHIEU PC ON SC.MaRap = PC.MaRap AND SC.SoThuTuPhong = PC.SoThuTuPhong
-    WHERE 
-        P.TenPhim LIKE N'%' + @p_TenPhim + N'%'  -- Tìm kiếm gần đúng tên phim
-        AND SC.NgayChieu = @p_NgayChieu     
-    ORDER BY 
-        SC.ThoiGianBatDau ASC;                  
+    ------------------------------------------------------------
+    -- 1) Có phim + có suất đúng ngày @p_NgayChieu ?
+    ------------------------------------------------------------
+    IF EXISTS (
+        SELECT 1
+        FROM PHIM P
+        JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
+        WHERE 
+            P.TenPhim LIKE N'%' + @p_TenPhim + N'%'
+            AND SC.NgayChieu = @p_NgayChieu
+    )
+    BEGIN
+        -- Trường hợp 1: Phim đó có suất đúng ngày → trả về đúng ngày đó
+        SELECT 
+            P.MaPhim,
+            P.TenPhim,
+            R.TenRap,
+            PC.LoaiPhong,
+            SC.NgayChieu,
+            SC.ThoiGianBatDau,
+            SC.ThoiGianKetThuc,
+            SC.DinhDangChieu, 
+            SC.TrangThai
+        FROM PHIM P
+        JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
+        JOIN RAP R ON SC.MaRap = R.MaRap
+        JOIN PHONGCHIEU PC ON SC.MaRap = PC.MaRap AND SC.SoThuTuPhong = PC.SoThuTuPhong
+        WHERE 
+            P.TenPhim LIKE N'%' + @p_TenPhim + N'%'
+            AND SC.NgayChieu = @p_NgayChieu
+        ORDER BY 
+            SC.ThoiGianBatDau ASC;
+    END
+    ELSE IF EXISTS (
+        --------------------------------------------------------
+        -- 2) Tên phim có tồn tại, nhưng KHÔNG có suất đúng ngày
+        --------------------------------------------------------
+        SELECT 1
+        FROM PHIM P
+        JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
+        WHERE 
+            P.TenPhim LIKE N'%' + @p_TenPhim + N'%'
+    )
+    BEGIN
+        -- Trường hợp 2: Phim có trong hệ thống → trả về tất cả ngày chiếu của phim đó
+        SELECT 
+            P.MaPhim,
+            P.TenPhim,
+            R.TenRap,
+            PC.LoaiPhong,
+            SC.NgayChieu,
+            SC.ThoiGianBatDau,
+            SC.ThoiGianKetThuc,
+            SC.DinhDangChieu, 
+            SC.TrangThai
+        FROM PHIM P
+        JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
+        JOIN RAP R ON SC.MaRap = R.MaRap
+        JOIN PHONGCHIEU PC ON SC.MaRap = PC.MaRap AND SC.SoThuTuPhong = PC.SoThuTuPhong
+        WHERE 
+            P.TenPhim LIKE N'%' + @p_TenPhim + N'%'
+        ORDER BY 
+            SC.NgayChieu ASC,
+            SC.ThoiGianBatDau ASC;
+    END
+    ELSE IF EXISTS (
+        --------------------------------------------------------
+        -- 3) Tên phim không tồn tại, nhưng ngày chiếu có suất
+        --------------------------------------------------------
+        SELECT 1
+        FROM SUATCHIEU SC
+        WHERE SC.NgayChieu = @p_NgayChieu
+    )
+    BEGIN
+        -- Trường hợp 3: Không tìm thấy phim, nhưng ngày đó có suất
+        -- → trả về tất cả phim chiếu trong ngày đó
+        SELECT 
+            P.MaPhim,
+            P.TenPhim,
+            R.TenRap,
+            PC.LoaiPhong,
+            SC.NgayChieu,
+            SC.ThoiGianBatDau,
+            SC.ThoiGianKetThuc,
+            SC.DinhDangChieu, 
+            SC.TrangThai
+        FROM PHIM P
+        JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
+        JOIN RAP R ON SC.MaRap = R.MaRap
+        JOIN PHONGCHIEU PC ON SC.MaRap = PC.MaRap AND SC.SoThuTuPhong = PC.SoThuTuPhong
+        WHERE 
+            SC.NgayChieu = @p_NgayChieu
+        ORDER BY 
+            P.TenPhim ASC,
+            SC.ThoiGianBatDau ASC;
+    END
+    ELSE
+    BEGIN
+        --------------------------------------------------------
+        -- 4) Tên phim không tồn tại, ngày đó cũng không có suất
+        -- → trả về TẤT CẢ lịch chiếu
+        --------------------------------------------------------
+        SELECT 
+            P.MaPhim,
+            P.TenPhim,
+            R.TenRap,
+            PC.LoaiPhong,
+            SC.NgayChieu,
+            SC.ThoiGianBatDau,
+            SC.ThoiGianKetThuc,
+            SC.DinhDangChieu, 
+            SC.TrangThai
+        FROM PHIM P
+        JOIN SUATCHIEU SC ON P.MaPhim = SC.MaPhim
+        JOIN RAP R ON SC.MaRap = R.MaRap
+        JOIN PHONGCHIEU PC ON SC.MaRap = PC.MaRap AND SC.SoThuTuPhong = PC.SoThuTuPhong
+        ORDER BY 
+            SC.NgayChieu ASC,
+            SC.ThoiGianBatDau ASC,
+            P.TenPhim ASC;
+    END
 END;
 GO
+
 
 
 /*
@@ -73,16 +178,24 @@ END;
 GO
 
 
-
 EXEC sp_TraCuuLichChieu
     @p_TenPhim = N'TỬ CHIẾN TRÊN KHÔNG',
     @p_NgayChieu = '2025-11-20';
-SELECT * FROM PHIM;
-SELECT * FROM SUATCHIEU;
 
-SELECT * FROM GIAODICH;
+EXEC sp_TraCuuLichChieu
+    @p_TenPhim = N'TỬ CHIẾN TRÊN KHÔNG',
+    @p_NgayChieu = null;
+
+EXEC sp_TraCuuLichChieu
+    @p_TenPhim = null,
+    @p_NgayChieu = '2025-11-20';
+
+EXEC sp_TraCuuLichChieu
+    @p_TenPhim = null,
+    @p_NgayChieu = null;
+
 EXEC sp_BaoCaoDoanhThuRap_ThucTe
     @p_TuNgay = '2025-11-17',
-    @p_DenNgay = '2025-11-23',
-    @p_DoanhThuMoc = 300000,
-    @p_KieuLoc = 2;
+    @p_DenNgay = '2025-11-30',
+    @p_DoanhThuMoc = 400000,
+    @p_KieuLoc = 1;
